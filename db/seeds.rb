f7a25b00c26131b27a11631af775c07f8b30a8ad
markdenardo <mark.denardo@gmail.com>
2020-04-28 23:43:04 -0400

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Movie.destroy_all
 
-m1 = Movie.create(name: "Shadows of Forgotten Ancestors", url: "https://www.youtube.com/watch?v=rb6GMSG2cEY", dir: "Sergei Parajanov", year:1965 )
-m2 = Movie.create(name: "Color of Pomegranates", url:"https://www.youtube.com/watch?v=26tEfblGH5I", dir: "Sergei Parajanov", year:1969 )
+m1 = Movie.create(name: "Shadows of Forgotten Ancestors", url: "https://www.youtube.com/watch?v=OeIzpWNG23Y", dir: "Sergei Parajanov", year:1965 )
+m2 = Movie.create(name: "Color of Pomegranates", url:"https://www.youtube.com/watch?v=lAurgH4G7Tg", dir: "Sergei Parajanov", year:1969 )
 m3 = Movie.create(name: "The Beales of Grey Gardens", url:"https://www.youtube.com/watch?v=nYpM7Srz5AQ", dir: "Albert and David Maysles", year:2006 )
-m4 = Movie.create(name: "Breathless", url:"https://www.youtube.com/watch?v=5RkH3V_MAs8", dir: "Jean Luc Godard", year:1960 )
+m4 = Movie.create(name: "Breathless", url:"https://www.youtube.com/watch?v=WCDEAu4R8hA", dir: "Jean Luc Godard", year:1960 )
 m5 = Movie.create(name: "In the Realms of the Unreal", url:"https://www.youtube.com/watch?v=sRlvDKcDvsI", dir: "Jessica Yu", year:2004 )
 m6 = Movie.create(name: "A Touch of Zen", url:"https://www.youtube.com/watch?v=18yH_9ZT27Q", dir: "King Hu", year:1971 )
 m7 = Movie.create(name: "Come Drink With Me", url:"https://www.youtube.com/watch?v=z1A6PnPZ6Gk", dir: "King Hu", year:1966 )
